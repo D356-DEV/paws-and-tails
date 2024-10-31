84f65ebc7c6fa05bc54179ca92569151d66b2af8
ulises-ramirez <uli.ram.oli@gmail.com>
2024-10-30 21:51:13 -0600

    Agregue la pagina de faq

---
+++ pages/faq/index/faq.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="stilos.css">
+
+</head>
+<body>
+    <main>
+        <section id="faq" class="faq">
+            <div class="accordion container my-5" id="accordionExample">
+                <h2 style="text-align: center;" class="my-4">FAQ</h2>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button" type="button" data-bs-toggle="collapse" 
+                        data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne"
+                        style="background-color: #BC7C7C; color: white;">
+                            ¿Cual es nuestra funcion?
+                        </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                        <div class="accordion-body" style="background-color: #E4C087; color: white;">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam commodi in cum aliquid cumque ut illo,
+                             architecto aliquam nostrum non incidunt unde id laudantium excepturi impedit quia ab soluta sunt?
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                         data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"
+                         style="background-color: #BC7C7C; color: white;">
+                            ¿Que nos hace diferentes?
+                        </button>
+                    </h2>
+                    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body" style="background-color: #E4C087; color: white;">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt rerum error excepturi unde nulla odio officia repudiandae optio ea ab exercitationem, placeat dolores assumenda nihil nemo fugit nam consectetur explicabo!
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" 
+                        data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"
+                        style="background-color: #BC7C7C; color: white;">
+                            ¿Como apoyamos a los refugios?
+                        </button>
+                    </h2>
+                    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body" style="background-color: #E4C087; color: white;">
+                            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element.
+                        </div>
+                    </div>
+                </div>
+                <div class="accordion-item">
+                    <h2 class="accordion-header">
+                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" 
+                        data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour"
+                        style="background-color: #BC7C7C; color: white;">
+                            Example #4
+                        </button>
+                    </h2>
+                   
+                    <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                        <div class="accordion-body" style="background-color: #E4C087; color: white;">
+                            uyvadvryKNC
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer class="container-fluid">
+            PAWS AND TAILS S.A. DE C.V.   COPYRIGHT 2024
+        </footer>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
